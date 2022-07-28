504476570da71bb8f2669f386e14eb133ff342f3
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-28 10:10:17 +0200

    commit creartabla

--- TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio9.java
+++ TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio9.java
@@ -1,5 +1,32 @@
 package Ejercicios;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Ejercicio9 {
 
+}
+
+// Método para crear tabla
+public static void createTable(String database, String name, String Query) {
+	// Hacemos un try para conectarnos, usar la database, y crear la tabla
+	try {
+		
+		// Usamos la tabla
+		String Querydatabase = "use" + database + ";";
+		Statement stdatabase = connection.createStatement();
+		stdatabase.executeUpdate(Querydatabase);
+		
+		// Creamos la tabla
+		
+		Statement st = connection.createStatement();
+		st.executeUpdate(Query);
+		System.out.println("Tabla creada con éxito");
+		
+		// Hacemos un catch en caso de que haya error en crear la tabla
+		
+	} catch (SQLException ex) {
+		System.out.println(ex.getMessage(ex));
+		System.out.println("Error creando tabla");
+	}
 }
