fce507b2f0c4a718204c8a44726b58144b8d02d1
IzanDuesoLopez <izandueso@gmail.com>
2022-07-28 10:19:13 +0200

    C

--- TA18 - Conexion Java-MySQL/src/database/BaseDeDatos.java
+++ TA18 - Conexion Java-MySQL/src/database/BaseDeDatos.java
@@ -4,7 +4,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.swing.JOptionPane;
 
 import com.mysql.cj.protocol.a.MysqlBinaryValueDecoder;
@@ -50,6 +49,28 @@ public class BaseDeDatos {
 		}
 	}
 	
-	
+	// Método para crear tabla
+	public static void createTable(String database, String name, String Query) {
+		// Hacemos un try para conectarnos, usar la database, y crear la tabla
+		try {
+			
+			// Usamos la tabla
+			String Querydatabase = "use" + database + ";";
+			Statement stdatabase = connect.createStatement();
+			stdatabase.executeUpdate(Querydatabase);
+			
+			// Creamos la tabla
+			
+			Statement st = connect.createStatement();
+			st.executeUpdate(Query);
+			System.out.println("Tabla creada con éxito");
+			
+			// Hacemos un catch en caso de que haya error en crear la tabla
+			
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage(ex));
+			System.out.println("Error creando tabla");
+		}
+	}
 	
 }
