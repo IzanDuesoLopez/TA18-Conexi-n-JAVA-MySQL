3dc84da1d0b753113408ee0d7f7c1465aaad1d43
IzanDuesoLopez <izandueso@gmail.com>
2022-07-28 11:44:29 +0200

    Cambios

--- TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio1.java
+++ TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio1.java
@@ -1,5 +1,22 @@
 package Ejercicios;
 
-public class Ejercicio1 {
+import database.BaseDeDatos;
 
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		
+		funcionalidad();
+		
+	}
+	
+	public static void funcionalidad() {
+		String nameDataBase = "los_almacenes";
+		
+		BaseDeDatos base_de_datos = new BaseDeDatos();
+		base_de_datos.deleteDataBase(nameDataBase);
+		base_de_datos.createDataBase(nameDataBase);
+		
+	}
+	
 }
--- TA18 - Conexion Java-MySQL/src/database/BaseDeDatos.java
+++ TA18 - Conexion Java-MySQL/src/database/BaseDeDatos.java
@@ -13,9 +13,6 @@ import com.mysql.cj.xdevapi.Statement;
 public class BaseDeDatos {
 	
 	private static Connection connect;
-	private String localServer= "jdbc:mysql://192.168.1.138:9090?useTimezone=true&serverTimeZona=UTC";
-	private String localUser= "root";
-	private String localPass= "Cibtraseña123";
 	
 	// Default builder
 	public BaseDeDatos() {
@@ -25,22 +22,36 @@ public class BaseDeDatos {
 	// Method that recives a string and creates a database
 	public void createDataBase(String dbName) {
 		try {
+			connectMySql(); // Connecting to database
 			String query = "create database " + dbName; // Query to create the database
 			java.sql.Statement jst = connect.createStatement(); // We create the statement
 			jst.executeUpdate(query); // We execute the query
 			closeConnection(); // Close connection
-			connectDataBase(localServer, localUser, localPass); // Connecting to database
+			
 			System.out.println("The database has been created: " + dbName); // Print that the database has been created
 		} catch (SQLException e) {
 			Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, e);
 		}
 	}
 	
+	// Method that deletes a database
+	public void deleteDataBase(String dbName) {
+		try {
+			String query = "drop database " + dbName; // Query to create the database
+			java.sql.Statement jst = connect.createStatement(); // We create the statement
+			jst.executeUpdate(query); // We execute the query
+			closeConnection(); // Close connection
+			System.out.println("The database has been deleted: " + dbName); // Print that the database has been created
+		} catch (SQLException e) {
+			Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, e);
+		}
+	}
+	
 	// Method for connecting to the database
-	public void connectDataBase(String localServer, String localUser, String localPass) {
+	public void connectMySql() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver"); // We use the driver
-			connect = DriverManager.getConnection(localServer, localUser, localPass); // We connect to our fedora server
+			connect = DriverManager.getConnection("jdbc:mysql://192.168.1.138:3306?useTimezone=true&serverTimezone=UTC", "local", "Local123@"); // We connect to our fedora server
 			System.out.println("You've been connected to the server"); // We print that we've been connected
 		} catch (SQLException | ClassNotFoundException ex) { // If some code returns error
 			System.out.println("Can't connect to database");
