3806b63321536022e56231bbe872d1f9793e79dc
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-05 10:27:58 +0200

    ejercicio 1 acabado

--- TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio1.java
+++ TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio1.java
@@ -1,83 +1,61 @@
 package Ejercicios;
 
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
-
-import com.mysql.cj.MysqlConnection;
-import com.mysql.cj.protocol.a.MysqlBinaryValueDecoder;
-import com.mysql.cj.xdevapi.Statement;
-
 import database.BaseDeDatos;
 
 public class Ejercicio1 {
-	connectDataBase();
-	createDataBase("Tienda_Informatica");
-	createTable("Tienda_Informatica", "Fabricantes", "(Codigo INT PRIMARY KEY AUTO_INCREMENT, Nombre NVARCHAR(100))");
-	createTable("Tienda_Informatica", "Fabricantes", "(Codigo INT PRIMARY KEY AUTO_INCREMENT, Nombre NVARCHAR(100), Precio INT, Fabricante INT)");
-	closeConnection();
-}
+	public static void main(String[] args) {
+		funcionalidad();
+	}
 
-//Method to create a table
-public static void createTable(String database, String name, String cosas) {
-			// We use a try to proceed with connection, use the database, and create the table
-			try {
-				
-				// We use the table
-				String querydatabase = "use" + database + ";";
-				java.sql.Statement stdatabase = connect.createStatement();
-				stdatabase.executeUpdate(querydatabase);
-				
-				// Creating the table
-				String query = "CREATE TABLE" + name + "" 
-						+ cosas;
-				java.sql.Statement st = connect.createStatement();
-				st.executeUpdate(query);
-				System.out.println("Tabla creada con éxito");
-				
-				// We do a catch in case an error exists
-			} catch (SQLException ex) {
-				System.out.println(ex.getMessage());
-				System.out.println("Error creando tabla");
-			}
-			
-			// Method that recives a string and creates a database
-			public void createDataBase(String dbName) {
-				try {
-					String query = "create database " + dbName; // Query to create the database
-					java.sql.Statement jst = connect.createStatement(); // We create the statement
-					jst.executeUpdate(query); // We execute the query
-					closeConnection(); // Close connection
-					connectDataBase(localServer, localUser, localPass); // Connecting to database
-					System.out.println("The database has been created: " + dbName); // Print that the database has been created
-				} catch (SQLException e) {
-					Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, e);
-				}
-			}
-			
-			// Method for connecting to the database
-			public void connectDataBase(String localServer, String localUser, String localPass) {
-				try {
-					Class.forName("com.mysql.cj.jdbc.Driver"); // We use the driver
-					connect = DriverManager.getConnection(localServer, localUser, localPass); // We connect to our fedora server
-					System.out.println("You've been connected to the server"); // We print that we've been connected
-				} catch (SQLException | ClassNotFoundException ex) { // If some code returns error
-					System.out.println("Can't connect to database");
-					System.out.println(ex);
-				}
-			}
-			
-			// Method for closing the connection
-			public void closeConnection() {
-				try {
-					connect.close(); // We close the connection
-					System.out.println("Connection ended");
-				} catch (SQLException e) {
-					Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, e);
-				}
-			}
+	public static void funcionalidad() {
+		String nameDataBase = "tienda_de_informatica";
+
+		// Query to create Fabricantes table
+		String fabricantes = "create table Fabricantes(codigo int auto_increment primary key,nombre nvarchar(100) not null);";
+		// Query to create Articulos table
+		String articulos = "create table Articulos(codigo int auto_increment primary key,precio int not null"+
+						"foreing key(fabricante) references Fabricantes(codigo));";
+		
+		int numAttributes = 3;
+		
+		// We define insert query variables for Fabricantes table
+		String insertFabricantesUno = "insert into Fabricantes(nombre) values ('Mercedes')";
+		String insertFabricantesDos = "insert into Fabricantes(nombre) values ('Red Bull')";
+		String insertFabricantesTres = "insert into Fabricantes(nombre) values ('Mc Claren')";
+		String insertFabricantesCuatro = "insert into Fabricantes(nombre) values ('Alpine')";
+		String insertFabricantesCinco = "insert into Fabricantes(nombre) values ('Williams')";
+		
+		// We define insert query variables for Articulos table
+		String insertArticulosUno = "insert into Articulos(fabricante, precio) values (1, 350)";
+		String insertArticulosDos = "insert into Articulos(fabricante, precio) values (2, 200)";
+		String insertArticulosTres = "insert into Articulos(fabricante, precio) values (3, 150)";
+		String insertArticulosCuatro = "insert into Articulos(fabricante, precio) values (4, 450)";
+		String insertArticulosCinco = "insert into Articulos(fabricante, precio) values (5, 600)";
+
+		BaseDeDatos base_de_datos = new BaseDeDatos(); // New object with the method defined
+		base_de_datos.connectMySql(); // Connect to MySql
+		base_de_datos.deleteDataBase(nameDataBase); // First we delete the actual database
+		base_de_datos.createDataBase(nameDataBase); // We create the database
+		base_de_datos.createTable(nameDataBase, fabricantes); // We create the table Fabricantes
+		base_de_datos.createTable(nameDataBase, articulos); // We create the table Articulos
+		
+		// We insert 5 registers into Fabricantes
+		base_de_datos.insertData(nameDataBase, insertFabricantesUno);
+		base_de_datos.insertData(nameDataBase, insertFabricantesDos);
+		base_de_datos.insertData(nameDataBase, insertFabricantesTres);
+		base_de_datos.insertData(nameDataBase, insertFabricantesCuatro);
+		base_de_datos.insertData(nameDataBase, insertFabricantesCinco);
+		
+		// We insert 5 registers into Articulos
+		base_de_datos.insertData(nameDataBase, insertArticulosUno);
+		base_de_datos.insertData(nameDataBase, insertArticulosDos);
+		base_de_datos.insertData(nameDataBase, insertArticulosTres);
+		base_de_datos.insertData(nameDataBase, insertArticulosCuatro);
+		base_de_datos.insertData(nameDataBase, insertArticulosCinco);
+		
+		// We print table registers
+		base_de_datos.getValues(nameDataBase, "Fabricantes", numAttributes);
+		base_de_datos.getValues(nameDataBase, "Articulos", numAttributes);
+	}
 }
 
