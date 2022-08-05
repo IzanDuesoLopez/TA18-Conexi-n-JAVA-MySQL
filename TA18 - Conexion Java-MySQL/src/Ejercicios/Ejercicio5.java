61e3e7ef3fe833f428c4568ebb0fe99305b52446
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-05 17:58:22 +0200

    Commit ejercicio 5 acabado

--- TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio5.java
+++ TA18 - Conexion Java-MySQL/src/Ejercicios/Ejercicio5.java
@@ -18,25 +18,25 @@ public class Ejercicio5 {
 		int numAttributes = 3;
 		
 		// We define insert query variables for Directores table
-		String insertDirectoresUno = "insert into Directores values ('Hitman', 18)";
-		String insertDirectoresDos = "insert into Directores values ('Dragon ball', 16)";
-		String insertDirectoresTres = "insert into Directores values ('Spongebob', 7)";
-		String insertDirectoresCuatro = "insert into Directores values ('Thor', 12)";
-		String insertDirectoresCinco = "insert into Directores values ('Spiderman', 12)";
+		String insertDirectoresUno = "insert into Directores values ('37430021', 'Pedro Valdivia', '326X')";
+		String insertDirectoresDos = "insert into Directores values ('38960330', 'Enrique Bunbury', '968F')";
+		String insertDirectoresTres = "insert into Directores values ('35322121', 'Rosa Garcia', '554E')";
+		String insertDirectoresCuatro = "insert into Directores values ('99620021', 'Ramon Melendi', '225M')";
+		String insertDirectoresCinco = "insert into Directores values ('7882561', 'Oscar Plana', '447C')";
 		
 		// We define insert query variables for Despachos table
-		String insertDespachosUno = "insert into Despachosvalues (1, 'Hitman')";
-		String insertDespachosDos = "insert into Despachos values (2, 'Dragon ball')";
-		String insertDespachosTres = "insert into Despachos values (3, 'Spongebob')";
-		String insertDespachosCuatro = "insert into Despachos values (4, 'Thor')";
-		String insertDespachosCinco = "insert into Despachos values (5, 'Spiderman')";
+		String insertDespachosUno = "insert into Despachosvalues (25000)";
+		String insertDespachosDos = "insert into Despachos values (37880)";
+		String insertDespachosTres = "insert into Despachos values (55220)";
+		String insertDespachosCuatro = "insert into Despachos values (77852)";
+		String insertDespachosCinco = "insert into Despachos values (33688)";
 
 		BaseDeDatos base_de_datos = new BaseDeDatos(); // New object with the method defined
 		base_de_datos.connectMySql(); // Connect to MySql
 		base_de_datos.deleteDataBase(nameDataBase); // First we delete the actual database
 		base_de_datos.createDataBase(nameDataBase); // We create the database
-		base_de_datos.createTable(nameDataBase, peliculas); // We create the table Directores
-		base_de_datos.createTable(nameDataBase, salas); // We create the table Despachos
+		base_de_datos.createTable(nameDataBase, directores); // We create the table Directores
+		base_de_datos.createTable(nameDataBase, despachos); // We create the table Despachos
 		
 		// We insert 5 registers into Peliculas
 		base_de_datos.insertData(nameDataBase, insertDirectoresUno);
